70608a2c1c60c8efeb03620abedfad6e398bf9cd
Tiago Caeiro <tiago.caeiro@vortal.biz>
2024-05-20 18:17:16 +0100

    add table config

--- NOS.Engineering.Challenge/Database/ApplicationDbContext.cs
+++ NOS.Engineering.Challenge/Database/ApplicationDbContext.cs
@@ -19,7 +19,42 @@ namespace NOS.Engineering.Challenge.Database
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Content>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.SubTitle)
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.ImageUrl)
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.Duration)
+                    .IsRequired();
+
+                entity.Property(e => e.StartTime)
+                    .IsRequired();
+
+                entity.Property(e => e.EndTime)
+                    .IsRequired();
+
+                // Configuration for GenreList can be more complex depending on how you want to store it.
+                // Here is an example of storing it as a JSON string
+                entity.Property(e => e.GenreList)
+                    .HasConversion(
+                        v => string.Join(',', v),
+                        v => v.Split(',', StringSplitOptions.RemoveEmptyEntries));
+            });
         }
     }
 }
